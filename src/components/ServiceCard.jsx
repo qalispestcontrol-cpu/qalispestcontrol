061cae616fa3118242d15414ba2603cf418189ae
xhantisolani <solanixhanti@gmail.com>
2025-12-21 16:14:03 +0200

    ui: increase ServiceCard image height (base:140px, md:220px)

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,7 +4,7 @@ import { Box, Heading, Text, Image } from '@chakra-ui/react'
 export default function ServiceCard({ img, title, desc }){
   return (
     <Box p={4} borderRadius="md" bg="white" boxShadow="sm" textAlign="left">
-      <Box mb={3} borderRadius="8px" overflow="hidden" height={{ base: '110px', md: '140px' }}>
+      <Box mb={3} borderRadius="8px" overflow="hidden" height={{ base: '140px', md: '220px' }}>
         <Image src={img} alt={title} width="100%" height="100%" objectFit="cover" loading="lazy" />
       </Box>
       <Heading size="sm" mb={2}>{title}</Heading>
